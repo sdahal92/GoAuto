6bd31f80962beeee73dcda46e9c500ab79964588
Spandan Dahal <spandandahal101@gmail.com>
2024-12-07 16:19:43 -0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -4,13 +4,13 @@ import streamlit as st
 col1, col2, col3 = st.columns([1, 2, 1])  # Adjust column widths as needed
 
 with col1:
-    st.image("images/Go_Auto.jpg", width=100, caption="Go Auto Logo")  # Adjust width for smaller size
+    st.image("images/Go_Auto.jpg", width=130)  # Adjust width for smaller size
 
 with col2:
     st.markdown("<h1 style='text-align: center;'>Overview Page</h1>", unsafe_allow_html=True)  # Centered title
 
 with col3:
-    st.image("images/Norquest Logo.jpeg", width=100, caption="Norquest Logo")  # Adjust width for smaller size
+    st.image("images/Norquest Logo.jpeg", width=130, )  # Adjust width for smaller size
 
 # Additional Content Below the Header
 st.write("Welcome to the Overview page of Go Norquest!")
