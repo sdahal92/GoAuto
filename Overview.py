c0963a105e389d4a9e7d571f237ab47220bb71f4
Spandan Dahal <spandandahal101@gmail.com>
2024-12-07 16:09:02 -0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.image("images/Go_Auto.jpg", caption="Image 1: Example Photo", use_column_width=True)
-st.image("images/Norquest Logo.jpeg", caption="Image 2: Example Photo", use_column_width=True)
+st.image("images/Go_Auto.jpg",use_column_width=True)
+st.image("images/Norquest Logo.jpeg",use_column_width=True)
 st.title("Overview Page")
 st.write("Welcome to the Overview page of Go Norquest!")
 
