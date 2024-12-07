67f090482202e0b8fe153efb14c110bd690abd33
Spandan Dahal <spandandahal101@gmail.com>
2024-12-07 15:33:50 -0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -4,6 +4,3 @@ import streamlit as st
 st.title("Overview Page")
 st.write("Welcome to the Overview page of Go Norquest!")
 
-# Add NorQuest College logo
-logo_url = "https://www.bher.ca/sites/default/files/images/2023-04/Norquest_CMYK.png"
-st.image(logo_url, caption="NorQuest College Logo", use_column_width=True)
