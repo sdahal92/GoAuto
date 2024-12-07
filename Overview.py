feb1d28f2cc3357932b27db862e340825a68ad14
Spandan Dahal <spandandahal101@gmail.com>
2024-12-07 16:02:43 -0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.image("image.png", caption="NorQuest College Logo", use_column_width=True)
 st.title("Overview Page")
 st.write("Welcome to the Overview page of Go Norquest!")
 
