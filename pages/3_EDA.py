b053d6c6f2b504f1fc6a0d01e519635d22eb51e1
Spandan Dahal <spandandahal101@gmail.com>
2024-12-08 21:08:56 -0700

    Update 3_EDA.py

--- pages/3_EDA.py
+++ pages/3_EDA.py
@@ -10,7 +10,7 @@ st.title("Exploratory Data Analysis (EDA)")
 st.markdown("""
     <iframe 
         src="https://app.powerbi.com/reportEmbed?reportId=33e3725d-78e3-40d8-8c93-f414f39a832c&autoAuth=true&ctid=2ba011f1-f50a-44f3-a200-db3ea74e29b7" 
-        width="120%" 
+        width="100px" 
         height="800px" 
         frameborder="0" 
         allowFullScreen="true">
