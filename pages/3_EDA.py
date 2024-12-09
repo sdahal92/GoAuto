a037ae99a21ddf8992b414ac62b0789a87dc764a
Spandan Dahal <spandandahal101@gmail.com>
2024-12-08 22:30:51 -0700

    Update 3_EDA.py

--- pages/3_EDA.py
+++ pages/3_EDA.py
@@ -25,13 +25,13 @@ st.markdown("""
     <div style="font-size: 24px; line-height: 1.8; color: #333;">
         <p>This bar chart shows the number of vehicles by fuel type in the dataset.</p>
         <ul>
-            <li><b>Gasoline Vehicles</b>: The majority, with 124,029 cars, dominate the dataset.</li>
-            <li><b>Diesel</b>: There are 8,632 diesel cars, the second-largest category.</li>
-            <li><b>Electric</b>: 5,023 electric vehicles reflect growing interest, though still smaller than gas and diesel.</li>
-            <li><b>Hybrid and PHEV</b>: 4,442 hybrid and 2,979 plug-in hybrids indicate some demand for fuel-efficient options.</li>
-            <li><b>CNG and Hydrogen</b>: These fuel types are rare, with only 6 CNG and 3 hydrogen vehicles.</li>
+            <listyle="font-size: 20px;><b>Gasoline Vehicles</b>: The majority, with 124,029 cars, dominate the dataset.</li>
+            <listyle="font-size: 20px;><b>Diesel</b>: There are 8,632 diesel cars, the second-largest category.</li>
+            <listyle="font-size: 20px;><b>Electric</b>: 5,023 electric vehicles reflect growing interest, though still smaller than gas and diesel.</li>
+            <listyle="font-size: 20px;><b>Hybrid and PHEV</b>: 4,442 hybrid and 2,979 plug-in hybrids indicate some demand for fuel-efficient options.</li>
+            <listyle="font-size: 20px;><b>CNG and Hydrogen</b>: These fuel types are rare, with only 6 CNG and 3 hydrogen vehicles.</li>
         </ul>
         <h3 style="font-size: 24px; font-weight: bold; color: #5C4033;">Conclusion:</h3>
-        <p>Gasoline vehicles are the largest group, with other fuel types present in much smaller numbers, highlighting the continued dominance of traditional fuel in the market.</p>
+        <pstyle="font-size: 20px;>Gasoline vehicles are the largest group, with other fuel types present in much smaller numbers, highlighting the continued dominance of traditional fuel in the market.</p>
     </div>
 """, unsafe_allow_html=True)
