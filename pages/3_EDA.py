d8f1321e94343b8db31ecec78f890b77c03a0cd8
Spandan Dahal <spandandahal101@gmail.com>
2024-12-08 22:29:30 -0700

    Update 3_EDA.py

--- pages/3_EDA.py
+++ pages/3_EDA.py
@@ -18,11 +18,11 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Display the image with adjusted width
-st.image("images/Picture5.jpg",  width=800)
+st.image("images/Picture5.jpg", width=800)
 
-# Description for the chart with larger font
+# Description for the chart with font size set to 24px
 st.markdown("""
-    <div style="font-size:24px ">
+    <div style="font-size: 24px; line-height: 1.8; color: #333;">
         <p>This bar chart shows the number of vehicles by fuel type in the dataset.</p>
         <ul>
             <li><b>Gasoline Vehicles</b>: The majority, with 124,029 cars, dominate the dataset.</li>
@@ -31,7 +31,7 @@ st.markdown("""
             <li><b>Hybrid and PHEV</b>: 4,442 hybrid and 2,979 plug-in hybrids indicate some demand for fuel-efficient options.</li>
             <li><b>CNG and Hydrogen</b>: These fuel types are rare, with only 6 CNG and 3 hydrogen vehicles.</li>
         </ul>
-        <h3 style="font-size: 1.5rem; font-weight: bold; color: #5C4033;">Conclusion:</h3>
+        <h3 style="font-size: 24px; font-weight: bold; color: #5C4033;">Conclusion:</h3>
         <p>Gasoline vehicles are the largest group, with other fuel types present in much smaller numbers, highlighting the continued dominance of traditional fuel in the market.</p>
     </div>
 """, unsafe_allow_html=True)
